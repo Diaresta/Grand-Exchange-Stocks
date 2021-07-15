892001a20b031f7ca05cf9a7099581d2c18a84d4
Andrew Robles <TheAndrewRobles@yahoo.com>
2021-07-15 15:21:56 -0400

    Log in page styling

--- client/src/components/Log-In.js
+++ client/src/components/Log-In.js
@@ -1,41 +1,46 @@
 const LogIn = () => {
   return (
-    <div>
+    <div id='log-sign-page'>
       <div class='log-sign-container'>
-        <h1>Log In</h1>
-        <form>
-          {/* <input type='text' placeholder='First Name...'></input>
+        <div class='form-container'>
+          <h1>Log In</h1>
+          <form>
+            {/* <input type='text' placeholder='First Name...'></input>
         <input type='text' placeholder='Last Name...'></input> */}
-          <input type='text' placeholder='Username'></input>
-          <input type='text' placeholder='Password'></input>
-          <button class=''>Log In</button>
-        </form>
+            <input type='text' placeholder='Username'></input>
+            <input type='text' placeholder='Password'></input>
+            <br />
+            <button class=''>Log In</button>
+          </form>
+        </div>
       </div>
-      <div>
+      <div class='log-sign-footer'>
         <a href=''>Forgot Password?</a>
         <a href=''>Sign Up!</a>
-        <a href=''>Privact Policy</a>
+        <a href=''>Privacy Policy</a>
       </div>
-
+      {/* 
       <div class='log-sign-container'>
         <h1>Create Account</h1>
         <form>
-          <input type='text' placeholder='First Name'></input>
-          <input type='text' placeholder='Last Name'></input>
-          <input type='text' placeholder='E-Mail'></input>
+          <div>
+            <input type='text' placeholder='First Name'></input>
+            <input type='text' placeholder='Last Name'></input>
+          </div>
           <input type='text' placeholder='Username'></input>
           <input type='text' placeholder='Password'></input>
+          <input type='text' placeholder='E-Mail'></input>
           <input type='' placeholder='Month'></input>
           <input type='' placeholder='Date'></input>
-          <input type='' placeholder='Year'></input>
+          <input type='date' placeholder='birthday'></input>
           <button class=''>Sign Up</button>
         </form>
       </div>
-      <div>
+      <div class='log-sign-footer'>
         <a href=''>Forgot Password?</a>
         <a href=''>Sign Up!</a>
-        <a href=''>Privact Policy</a>
-      </div>
+        <a href=''>Privacy Policy</a>
+      </div> */}
     </div>
   );
 };
