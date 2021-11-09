98e8fc62abc2287a496e301e65f631666b4115fe
Andrew Robles <TheAndrewRobles@yahoo.com>
2021-11-09 00:21:31 -0500

    Search page update. Added page position jump

--- client/src/components/Search-Page.js
+++ client/src/components/Search-Page.js
@@ -42,24 +42,112 @@ const SearchPage = () => {
 
   return (
     <div id='search-container'>
-      <p>Search Results: '{query}'</p>
-      <small>{filteredItems.length} Results</small>
-      {/* <small>REEE</small> */}
+      <div id='query-container'>
+        <h2>Search Results: '{query}'</h2>
+        <small>{filteredItems.length} Results</small>
+        {/* <small>REEE</small> */}
 
-      <ul id='item-search-ul'>
-        {filteredItems.map((item) => (
-          <Link
-            to={`/item/${item.name}/${item.id}`}
-            className='item-search-li'
-            key={item.id}
-            onClick={() => sendItemToSever(item.id)}
-          >
-            <li key={item.id}>
-              {item.name[0].toUpperCase() + item.name.slice(1)}
-            </li>
-          </Link>
-        ))}
-      </ul>
+        <ul id='item-search-ul'>
+          {filteredItems.map((item) => (
+            <Link
+              to={`/item/${item.name}/${item.id}`}
+              className='item-search-li'
+              key={item.id}
+              onClick={() => sendItemToSever(item.id)}
+            >
+              <li key={item.id} id={item.name[0]}>
+                {item.name[0].toUpperCase() + item.name.slice(1)}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div id='alphabet-container'>
+        <ul>
+          <li>
+            <a href='#3'>0</a>
+          </li>
+          <li>
+            <a href='#a'>A</a>
+          </li>
+          <li>
+            <a href='#b'>B</a>
+          </li>
+          <li>
+            <a href='#c'>C</a>
+          </li>
+          <li>
+            <a href='#d'>D</a>
+          </li>
+          <li>
+            <a href='#e'>E</a>
+          </li>
+          <li>
+            <a href='#f'>F</a>
+          </li>
+          <li>
+            <a href='#g'>G</a>
+          </li>
+          <li>
+            <a href='#h'>H</a>
+          </li>
+          <li>
+            <a href='#i'>I</a>
+          </li>
+          <li>
+            <a href='#j'>J</a>
+          </li>
+          <li>
+            <a href='#k'>K</a>
+          </li>
+          <li>
+            <a href='#l'>L</a>
+          </li>
+          <li>
+            <a href='#m'>M</a>
+          </li>
+          <li>
+            <a href='#n'>N</a>
+          </li>
+          <li>
+            <a href='#o'>O</a>
+          </li>
+          <li>
+            <a href='#p'>P</a>
+          </li>
+          <li>
+            <a href='#q'>Q</a>
+          </li>
+          <li>
+            <a href='#r'>R</a>
+          </li>
+          <li>
+            <a href='#s'>S</a>
+          </li>
+          <li>
+            <a href='#t'>T</a>
+          </li>
+          <li>
+            <a href='#u'>U</a>
+          </li>
+          <li>
+            <a href='#v'>V</a>
+          </li>
+          <li>
+            <a href='#w'>W</a>
+          </li>
+          <li>
+            <a href='#x'>X</a>
+          </li>
+          <li>
+            <a href='#y'>Y</a>
+          </li>
+          <li>
+            <a href='#z'>Z</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
