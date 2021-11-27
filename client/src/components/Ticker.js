813ee77c73026a066c801d85b89100611a167f0b
Andrew Robles <TheAndrewRobles@yahoo.com>
2021-11-26 19:10:57 -0500

    Added dynamic ticker item mapping. Change source from hard coded to api call

--- client/src/components/Ticker.js
+++ client/src/components/Ticker.js
@@ -1,16 +1,64 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Ticker = () => {
   const [tickerData, setTicker] = useState();
+  const [state, setstate] = useState();
+
+  const testBoys = [
+    {
+      name: 'Amulet of fury',
+      trend: 'negative',
+      price: '1.6m',
+      percent: '+12.0%',
+    },
+    {
+      name: 'Adamant platelegs',
+      trend: 'positive',
+      price: '3,492',
+      percent: '-1.0%',
+    },
+    {
+      name: 'Abyssal whip',
+      trend: 'neutral',
+      price: '1.8m',
+      percent: '+8.0%',
+    },
+    {
+      name: 'Armadyl godsword',
+      trend: 'neutral',
+      price: '12.4m',
+      percent: '+70.0%',
+    },
+    {
+      name: 'Bandos chestplate',
+      trend: 'negative',
+      price: '24.7m',
+      percent: '+65.0%',
+    },
+    {
+      name: 'Cannonball',
+      trend: 'neutral',
+      price: '182',
+      percent: '+4.0%',
+    },
+    {
+      name: 'Adamant platelegs',
+      trend: 'neutral',
+      price: '3,492',
+      percent: '-1.0%',
+    },
+  ];
 
   const tickerCall = async () => {
-    const url = await 'http://localhost:8000/';
-    const response = await fetch(url);
-    const data = await response.json();
+    const url = await axios.get('http://localhost:8000/');
+    // const response = await fetch(url);
+    const data = await url.data;
 
-    setTicker(data.ticker);
-    console.log(tickerData);
-    console.log(data.ticker);
+    setstate('trfyghb');
+
+    console.log(state);
+    // console.log(data.ticker);
   };
 
   useEffect(() => {
@@ -20,19 +68,17 @@ const Ticker = () => {
     //   setHighAlchColor('alch-neg');
     // }
     tickerCall();
-
-    console.log(tickerData);
   }, []);
   return (
     <div id='ticker-container'>
       <div id='moving-container'>
-        {/* {tickerData.map((item) => (
+        {testBoys.map((item) => (
           <p className={item.trend}>
             <strong>{item.name}</strong> - {item.price} ({item.percent})
           </p>
-        ))} */}
+        ))}
 
-        <p className='positive'>
+        {/* <p className='positive'>
           <strong>Adamant Boots</strong> - 200g (+2.4%)
         </p>
         <p className='negative'>
@@ -49,7 +95,7 @@ const Ticker = () => {
         </p>
         <p className='positive'>
           <strong>Magic Logs</strong> - 895g (+1.23%)
-        </p>
+        </p> */}
       </div>
     </div>
   );
