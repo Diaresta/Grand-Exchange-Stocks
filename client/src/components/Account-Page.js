86f1462c1aaacb3c7e3dfcc888fab2b2619e0457
Andrew Robles <TheAndrewRobles@yahoo.com>
2021-12-12 23:36:59 -0500

    Account page styling. Added rudimentary account page button functionality

--- client/src/components/Account-Page.js
+++ client/src/components/Account-Page.js
@@ -1,93 +1,161 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
 
 const AccountPage = ({ testName, testEmail }) => {
+  const [newEmail, setNewEmail] = useState('');
+  const [newEmailVerify, setNewEmailVerify] = useState('');
+
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordVerify, setNewPasswordVerify] = useState('');
+  const passwordPlaceholder = 'placeholder';
+
   return (
-    <div id='account-page'>
+    <div id='account-container'>
       <h2>Account Info</h2>
 
-      {/* <div className='log-sign-container'> */}
-      <table>
-        <tbody>
-          <tr>
-            <th>Account</th>
-          </tr>
-          <tr>
-            <td>Diaresta</td>
-          </tr>
-          <tr>
-            <th>Account ID</th>
-          </tr>
-          <tr>
-            <td>000000</td>
-          </tr>
-          <tr>
-            <th>Sign-up Date</th>
-          </tr>
-          <tr>
-            <td>06/30/2021 - 22:33:23</td>
-          </tr>
-          <tr>
-            <th>Email</th>
-          </tr>
-          <tr>
-            <td>testemail123.@gmail.com</td>
-          </tr>
-        </tbody>
-      </table>
+      <div id='account-page'>
+        <div id='table-left'>
+          <table>
+            <tbody>
+              <tr>
+                <th>Account</th>
+              </tr>
+              <tr>
+                <td>Diaresta</td>
+              </tr>
+              <tr>
+                <th>Account ID</th>
+              </tr>
+              <tr>
+                <td>000000</td>
+              </tr>
+              <tr>
+                <th>Sign-up Date</th>
+              </tr>
+              <tr>
+                <td>06/30/2021 - 22:33:23</td>
+              </tr>
+              <tr>
+                <th>Email</th>
+              </tr>
+              <tr>
+                <td>testemail123.@gmail.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-      <table>
-        <tbody>
-          <tr>
-            <th>Email</th>
-          </tr>
-          <tr>
-            <td>New Email:</td>
-            <td>
-              <input type='text' required />
-            </td>
-          </tr>
-          <tr>
-            <td>Verify Email:</td>
-            <td>
-              <input type='text' required />
-            </td>
-          </tr>
-        </tbody>
-        <button>Save</button>
-      </table>
+        <div id='table-right'>
+          <table>
+            <tbody>
+              <tr>
+                <th colspan='2'>Email</th>
+              </tr>
+              <tr>
+                <td>New Email:</td>
+                <td>
+                  <input
+                    type='text'
+                    value={newEmail}
+                    onInput={(e) => setNewEmail(e.target.value)}
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Verify Email:</td>
+                <td>
+                  <input
+                    type='text'
+                    value={newEmailVerify}
+                    onInput={(e) => setNewEmailVerify(e.target.value)}
+                    required
+                  />
+                </td>
+              </tr>
+            </tbody>
+            <button
+              type='submit'
+              onClick={() => {
+                if (newEmail !== newEmailVerify) {
+                  console.log(`New Emails Don't Match`);
+                } else if (newEmail === '' || newEmailVerify === '') {
+                  console.log('Email(s) Missing');
+                } else {
+                  console.log('Woo');
+                }
+              }}
+            >
+              Save
+            </button>
+          </table>
 
-      <table>
-        <tbody>
-          <thead>
-            <tr>
-              <th>Password</th>
-            </tr>
-          </thead>
-          <tr>
-            <td>Current Password:</td>
-            <td>
-              <input type='text' required />
-            </td>
-          </tr>
-          <tr>
-            <td>New Password:</td>
-            <td>
-              <input type='text' required />
-            </td>
-          </tr>
-          <tr>
-            <td>Verify New Password:</td>
-            <td>
-              <input type='text' required />
-            </td>
-          </tr>
-        </tbody>
-        <button>Save</button>
-      </table>
-      {/* </div> */}
+          <table>
+            <tbody>
+              <tr>
+                <th colspan='2'>Password</th>
+              </tr>
+              <tr>
+                <td>Current Password:</td>
+                <td>
+                  <input
+                    type='text'
+                    value={currentPassword}
+                    onInput={(e) => setCurrentPassword(e.target.value)}
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>New Password:</td>
+                <td>
+                  <input
+                    type='text'
+                    value={newPassword}
+                    onInput={(e) => {
+                      setNewPassword(e.target.value);
+                    }}
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Verify New Password:</td>
+                <td>
+                  <input
+                    type='text'
+                    value={newPasswordVerify}
+                    onInput={(e) => setNewPasswordVerify(e.target.value)}
+                    required
+                  />
+                </td>
+              </tr>
+            </tbody>
+            <button
+              type='submit'
+              onClick={() => {
+                if (currentPassword !== passwordPlaceholder) {
+                  console.log('Wrong Password');
+                } else if (
+                  currentPassword === '' ||
+                  newPassword === '' ||
+                  newPasswordVerify === ''
+                ) {
+                  console.log('Password(s) Missing');
+                } else if (newPassword !== newPasswordVerify) {
+                  console.log(`New Passwords Don't Match`);
+                } else console.log('Woo');
+              }}
+            >
+              Save
+            </button>
+          </table>
+        </div>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 };
