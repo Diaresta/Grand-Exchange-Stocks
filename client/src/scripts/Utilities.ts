cd51293da42e84c883a4e200a85481f0f3932af5
Diaresta <TheAndrewRobles@yahoo.com>
2024-07-07 00:17:01 -0400

    Added items name-id json. Added scripts file with utility methods

---
+++ client/src/scripts/Utilities.ts
@@ -0,0 +1,20 @@
+const ITEM_NAME_ID_API: string =
+  'https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson';
+
+const fetchItems = async (item_api: string) => {
+  const response = await fetch(item_api);
+  const data = await response.json();
+  return data;
+};
+
+const createItemJSON = (items: any) => {
+  let output: { name: string; id: number }[] = [];
+
+  for (let i = 2; i < Object.keys(items).length; i++) {
+    output.push({
+      name: Object.keys(items)[i],
+      id: Object.values(items)[i] as number,
+    });
+  }
+  return output;
+};
