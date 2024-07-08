2d7bfe28c1aec58a46f5439bf40e434736fc3f4d
Diaresta <TheAndrewRobles@yahoo.com>
2024-07-08 00:54:32 -0400

    Added alphabet component with tests

---
+++ client/src/components/SearchPage/AlphabetSearchContainer.tsx
@@ -0,0 +1,21 @@
+export const ALPHABET = () => {
+  return Array.from({ length: 26 }, (_e, i) => String.fromCharCode(65 + i));
+};
+
+const AlphabetSearchContainer = () => {
+  return (
+    <div id='alphabet-container'>
+      <ul>
+        <li key={0}>
+          <a href='#3'>0</a>
+        </li>
+        {ALPHABET().map((letter: string) => (
+          <li key={letter}>
+            <a href={'#' + letter.toLowerCase()}>{letter}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default AlphabetSearchContainer;
